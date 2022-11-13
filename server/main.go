b918a18f69012fa8d17507ce51d31b8232a93b41
okd <y.okada.upd@gmail.com>
2022-11-13 23:38:33 +0900

    create:root api

--- server/main.go
+++ server/main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	}))
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Echo World!!")
 	})

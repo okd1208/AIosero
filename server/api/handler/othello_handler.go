07407efad2894e2683fc313f9248825b90114f91
okd <y.okada.upd@gmail.com>
2023-07-21 23:05:30 +0900

    feat: GetValidMoves algorithm

--- server/api/handler/othello_handler.go
+++ server/api/handler/othello_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -13,8 +14,8 @@ type OthelloRequest struct {
 }
 
 type NextMove struct {
-	row int
-	col int
+	Row int `json:"row"`
+	Col int `json:"col"`
 }
 
 var clientColor = 1
@@ -29,6 +30,20 @@ func GetNextMovePosition(req OthelloRequest) (bestMove NextMove, err error) {
 	matrix := positionsToMatrix(req.Positions, req.UserColor)
 
 	// TODO:決定木に基づいてnextMoveを算出
+
+	validMoves := GetValidMoves(matrix)
+	bestScore := -1
+	for _, point := range validMoves {
+		x := point[0]
+		y := point[1]
+		score := evaluateMove(matrix, x, y, computeColor)
+		if score > bestScore {
+			bestScore = score
+			bestMove = NextMove{Row: x, Col: y}
+		}
+	}
+
+	fmt.Println(bestMove)
 
 	return bestMove, nil
 }
@@ -77,4 +92,53 @@ func positionsToMatrix(positions map[string]map[string][]int, userColor string) 
 		}
 	}
 	return matrix
+}
+
+func GetValidMoves(matrix [][]int) [][]int {
+	validMoves := make([][]int, 0)
+	directions := [8][2]int{
+		{-1, -1}, // 左上
+		{-1, 0},  // 上
+		{-1, 1},  // 右上
+		{0, -1},  // 左
+		{0, 1},   // 右
+		{1, -1},  // 左下
+		{1, 0},   // 下
+		{1, 1},   // 右下
+	}
+
+	for x := 0; x < len(matrix); x++ {
+		for y := 0; y < len(matrix[x]); y++ {
+			if matrix[x][y] != 0 {
+				continue
+			}
+			for _, direction := range directions {
+				dx, dy := direction[0], direction[1]
+				if CheckMoveInDirection(matrix, x, y, dx, dy) {
+					validMoves = append(validMoves, []int{x, y})
+				}
+			}
+		}
+	}
+	return validMoves
+}
+
+// CheckMoveInDirectionは、与えられた方向(dx, dy)に対して石を置けるかどうかを探索する
+// x, y は開始位置、dx, dyは探索方向です（-1, 0, 1のいずれか）
+func CheckMoveInDirection(matrix [][]int, x, y, dx, dy int) bool {
+	var isEnemyAdjacent bool // 敵の石が隣接しているかどうか
+	x, y = x+dx, y+dy
+	for x >= 0 && x < len(matrix) && y >= 0 && y < len(matrix[x]) {
+		if matrix[x][y] == 0 {
+			return false
+		}
+		if matrix[x][y] == computeColor {
+			return isEnemyAdjacent
+		}
+		if matrix[x][y] == clientColor && !isEnemyAdjacent {
+			isEnemyAdjacent = true
+		}
+		x, y = x+dx, y+dy
+	}
+	return false
 }

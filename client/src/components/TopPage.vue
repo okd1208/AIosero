f5ed2973a1b158647b1661469df4ea180fec559b
okd <y.okada.upd@gmail.com>
2023-07-22 17:50:39 +0900

    refactor: delete unusecode

--- client/src/components/TopPage.vue
+++ client/src/components/TopPage.vue
@@ -1,27 +1,19 @@
 <template>
   <div class="othelloboad">
     <h1>Leaning Othello</h1>
-    <button @click="Send">PlayOthello</button>
-    <router-link to="/playBoard">play</router-link>
+    <router-link to="/playBoard">PlayOthello</router-link>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
 export default {
-  name: 'HelloWorld',
+  name: 'SelectMenu',
   data: function () {
     return {
       info: null,
     }
   },
   methods: {
-    async Send() {
-      axios
-      .get('http://localhost:8888')
-      .then(response => (this.info = response))
-      console.log(this.info);
-    }
   }
 }
 </script>
